1b7bacc5aedce7388bd135d02e50b967641da5f2
Anissa Jones <anissajones10@gmail.com>
2016-06-17 15:56:27 -0400

    Adds a has_many through relation between users and facilities.

--- app/models/facility.rb
+++ app/models/facility.rb
@@ -1,5 +1,7 @@
 class Facility < ActiveRecord::Base
   has_many :animals
+  has_many :staff
+  has_many :users, through: :staff
 
   validates :name, presence: true
 end
---
+++ app/models/staff.rb
@@ -0,0 +1,4 @@
+class Staff < Setting
+  belongs_to :user
+  belongs_to :facility_id
+end
