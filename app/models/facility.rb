ed926f6993478d8bc779ace755e71d9203c69db4
Dylan Pulliam <dpulliam@customink.com>
2016-06-17 13:50:44 -0400

    Update facility to accept nested attributes

--- app/models/facility.rb
+++ app/models/facility.rb
@@ -3,4 +3,6 @@ class Facility < ActiveRecord::Base
   has_many :pictures, :dependent => :destroy
 
   validates :name, presence: true
+
+  accepts_nested_attributes_for :pictures, :reject_if => lambda { |t| t['trip_image'].nil? }
 end
