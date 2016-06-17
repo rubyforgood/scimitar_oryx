b19006e45679246f975e1c0e3fcc779edd335496
Chris Mar <cmar@customink.com>
2016-06-17 13:07:19 -0400

    add simple_form to edit species for Animals

--- app/models/facility.rb
+++ app/models/facility.rb
@@ -1,2 +1,4 @@
 class Facility < ActiveRecord::Base
+
+  validates :name, presence: true
 end
--- spec/models/animal_spec.rb
+++ spec/models/animal_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Animal, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it 'requires a species' do
+    animal = Animal.new(name: "JC")
+    expect(animal.valid?).to be_falsey
+  end
 end
