a7c9079098e13672ccc8cf90b5bd98a720d675e8
Dylan Pulliam <dpulliam@customink.com>
2016-06-17 13:17:53 -0400

    Add Picture model and migration

---
+++ app/models/picture.rb
@@ -0,0 +1,17 @@
+class Picture < ActiveRecord::Base
+  belongs_to :animal
+  belongs_to :facility
+
+
+  # This method associates the attribute ":avatar" with a file attachment
+  has_attached_file :image, styles: {
+    thumb: '100x100>',
+    square: '200x200#',
+    medium: '300x300>',
+    large:   '400x400>'
+  }
+
+  # Validate the attached image is image/jpg, image/png, etc
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
+end
