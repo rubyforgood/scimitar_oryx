477b1f4da23727ac3097244bff063e5d5516d5d8
Dylan Pulliam <dpulliam@customink.com>
2016-06-17 13:20:52 -0400

    Add picture to animal

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -2,6 +2,8 @@ class Animal < ActiveRecord::Base
   belongs_to :facility
   belongs_to :species
 
+  has_many :pictures, :dependent => :destroy
+
   validates :name, presence: true
   validates :facility_id, presence: true
   validates :species_id, presence: true
