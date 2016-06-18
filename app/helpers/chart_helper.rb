d5ba3da2ba84431e7ae4993ac8c890aba17d2074
pachacamac <pachacamac@inboxalias.com>
2016-06-18 03:26:48 +0200

    generalize chart helper

--- app/helpers/chart_helper.rb
+++ app/helpers/chart_helper.rb
@@ -1,10 +1,11 @@
 module ChartHelper
 	def chart(type, data, opts={})
-		content_tag(:div, "",
-			class: type,
+		content_tag(:div, '',
+			class: 'chart',
 			id: (opts.delete(:id) || SecureRandom.uuid),
       data: {
-        data: {data: data}.to_json,
+        type: type,
+        data: data.to_json,
         options: opts.to_json
       }
 		).html_safe
