2efeb42926ff81c2ca1791b1c588332ac56fd5b0
pachacamac <pachacamac@inboxalias.com>
2016-06-18 17:08:35 +0200

    add interests to facilities

--- app/models/facility.rb
+++ app/models/facility.rb
@@ -11,4 +11,19 @@ class Facility < ActiveRecord::Base
   validates :name, presence: true
 
   accepts_nested_attributes_for :pictures
+
+  VALID_INTERESTS = {
+    'grow_herd_size'                  => 'Breed to grow herd size',
+    'maintain_herd_size'              => 'Breed to maintain herd size',
+    'decrease_herd_size'              => 'Looking to decrease herd size',
+    'house_post_reproductive_animals' => 'Willing to house post-reproductive animals',
+    'house_new_species'               => 'Interested in housing new species'
+  }.freeze
+
+  before_save { |facility| facility.interests = facility.interests.uniq.select{ |e| VALID_INTERESTS.keys.include?(e) } }
+
+  VALID_INTERESTS.keys.each do |interest|
+    define_method("interest_to_#{interest}?"){ interests.include?(interest) }
+  end
+
 end
