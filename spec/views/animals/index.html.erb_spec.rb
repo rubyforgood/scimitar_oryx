b1efeb990e546d1e2afc34085668410371f20415
Jeremy Warner <jeremywrnr@gmail.com>
2016-06-18 11:50:40 -0400

    pass index test
    pass spec for facility view index
    
    pass facility edit view
    
    passing facility show

--- spec/views/animals/index.html.erb_spec.rb
+++ spec/views/animals/index.html.erb_spec.rb
@@ -5,11 +5,9 @@ RSpec.describe "animals/index", type: :view do
   it "renders a list of animals" do
     assign(:animals, [
       create(:sample_animal),
-      create(:sample_animal),
     ])
 
     render
-
-    assert_select "div.Animal", :count => 2
+    assert_select "div.Animal", :count => 1
   end
 end
--- spec/views/animals/show.html.erb_spec.rb
+++ spec/views/animals/show.html.erb_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "animals/show", type: :view do
+
   let(:animal) { build :animal, :name => "Jeremy" }
 
   it "renders attributes in <p>" do
