527a1cb071fc2f9b86ac7cff625c445911091e15
pachacamac <pachacamac@inboxalias.com>
2016-06-18 22:30:57 +0200

    add helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,7 +10,7 @@ module ApplicationHelper
           name: 'SPA Style',
           styles: [
             {featureType: 'poi.attraction', stylers: [{color: '#fce8b2'}]},
-            {featureType: 'road.highway', stylers: [{hue: '#0277bd'}, {saturation: -50}]},
+            {featureType: 'road.highway', stylers: [{hue: '#8c8279'}, {saturation: -50}]},
             {featureType: 'road.highway', elementType: 'labels.icon', stylers: [{hue: '#000'}, {saturation: 100}, {lightness: 50}]},
             {featureType: 'landscape', stylers: [{hue: '#74aa50'}, {saturation: 10}, {lightness: -22}]}
           ]
