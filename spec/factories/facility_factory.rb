53650713c68847b6c81a2735c1ba5ec5aea4b1cc
Robert P. Churchill <products@rpchurchill.com>
2016-06-18 12:13:41 -0400

    add facility sample generation

--- spec/factories/facility_factory.rb
+++ spec/factories/facility_factory.rb
@@ -3,4 +3,19 @@ FactoryGirl.define do
     name "Smithsonian-Mason School of Conservation"
     facility_type
   end
+
+  factory :sample_facility , class: Facility do
+    name {Faker::Company.name}
+    facility_type_id {Faker::Number.between(1,3)}
+    spa {Faker::Boolean.boolean}
+    acreage {Faker::Number.between(1,50000)}
+    endangered_acreage {Faker::Number.between(1,50000)}
+    temperture_controled {Faker::Boolean.boolean}
+    water_features {Faker::Boolean.boolean}
+    supplemental_feed {Faker::Boolean.boolean}
+    facility_expertise_id {Faker::Number.between(1,5)}
+    address {Faker::Address.street_address}
+    longitude {Faker::Address.longitude}
+    latitude {Faker::Address.latitude}
+  end
 end
