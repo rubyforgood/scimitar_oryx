32ceca7244b51568a66ab94d0a93c1026943c9f8
pachacamac <pachacamac@inboxalias.com>
2016-06-18 04:24:46 +0200

    add env var fallbacks for development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,13 +37,16 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
+
+  env_var_missing = ->{ STDERR.puts 'ENV VAR MISSING! SHOULD\'NT MATTER THOUGH SINCE WE\'RE IN DEVELOPMENT'; 'xxx' }
+
   config.paperclip_defaults = {
     storage: :s3,
     s3_credentials: {
-      bucket: ENV.fetch('S3_BUCKET_NAME'),
-      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
-      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-      s3_region: ENV.fetch('AWS_REGION'),
+      bucket: ENV.fetch('S3_BUCKET_NAME', env_var_missing.call),
+      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID', env_var_missing.call),
+      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY', env_var_missing.call),
+      s3_region: ENV.fetch('AWS_REGION', env_var_missing.call),
     }
   }
 end
