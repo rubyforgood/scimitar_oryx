9c75bf87aa7cdd49ff5483a179c25e314957b331
Tien Nguyen <nht007@gmail.com>
2016-06-18 10:25:09 -0400

    Fix syntax error with facilities strong params

--- app/controllers/facilities_controller.rb
+++ app/controllers/facilities_controller.rb
@@ -72,6 +72,11 @@ class FacilitiesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def facility_params
-      params.require(:facility).permit(:name, pictures_attributes: [:name, :image], :facility_type_id, :facilities_users_attributes => [:user_id, :facility_id])
+      params.require(:facility).permit(
+        :name,
+        :facility_type_id,
+        pictures_attributes: [:name, :image],
+        facilities_users_attributes: [:user_id, :facility_id]
+      )
     end
 end
