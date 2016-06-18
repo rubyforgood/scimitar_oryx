a08e6165793a1b7d8f061efc99193517b7cb4438
pachacamac <pachacamac@inboxalias.com>
2016-06-18 04:47:25 +0200

    add basic chart styling options

--- app/assets/javascripts/charts.js
+++ app/assets/javascripts/charts.js
@@ -1,10 +1,17 @@
 $(document).ready(function(){
   google.charts.load('current', {'packages':['corechart', 'geochart']}); /* remember to add packages if you use fancy charts */
+  function mergeOptions(obj1,obj2){
+    var obj3 = {};
+    for(var attr in obj1){ obj3[attr] = obj1[attr]; }
+    for(var attr in obj2){ obj3[attr] = obj2[attr]; }
+    return obj3;
+  }
+  var baseOptions = {colors: ['#4F81BD', '#C0504D', '#9BBB59', '#8064A2', '#1F497D']};
   google.charts.setOnLoadCallback(function(){
     $('.chart').each(function(idx, obj) {
       new google.visualization[$(obj).data('type')](obj).draw(
         google.visualization.arrayToDataTable($(obj).data('data')),
-        $(obj).data('options')
+        mergeOptions(baseOptions, $(obj).data('options'))
       );
     });
   });
