f28b5929c373bb22bc1d98c998ea621207de9e36
Anissa Jones <anissajones10@gmail.com>
2016-06-18 14:16:36 -0400

    no change to schema, just pulled down.

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20160618152843) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,12 +55,9 @@ ActiveRecord::Schema.define(version: 20160618152843) do
     t.boolean  "supplemental_feed"
     t.integer  "facility_expertise_id"
     t.text     "interests",             default: ["grow_herd_size"],              array: true
-<<<<<<< HEAD
-=======
     t.string   "address"
     t.float    "latitude"
     t.float    "longitude"
->>>>>>> master
   end
 
   add_index "facilities", ["facility_expertise_id"], name: "index_facilities_on_facility_expertise_id", using: :btree
@@ -95,6 +90,8 @@ ActiveRecord::Schema.define(version: 20160618152843) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_index "settings", ["type"], name: "index_settings_on_type", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "",    null: false
