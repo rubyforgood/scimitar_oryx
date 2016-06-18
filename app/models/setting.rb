a2386eab4d4fa785abeaa7ec39f854983501f2af
JC Avena <jcavena@gmail.com>
2016-06-18 11:30:38 -0400

    Scope name uniqueness for Settings model.

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -1,5 +1,5 @@
 class Setting < ActiveRecord::Base
   acts_as_list scope: [:type]
   
-  validates :name, presence: true, uniqueness: { case_sensitive: false }
+  validates :name, presence: true, uniqueness: { case_sensitive: false, scope: :type }
 end
