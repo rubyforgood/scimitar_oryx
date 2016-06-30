0fa9ef94ac8941da9c1cdc09c4f482bc88552d2e
JC Avena <jcavena@gmail.com>
2016-06-30 16:20:19 -0500

    I'm starting to dislike houndci. Seems will spend more time fixing whitespace than I care to.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,7 +38,7 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
 
-  env_var_missing = -> { "xxx".tap { STDERR.puts "ENV VAR MISSING! SHOULDN'T MATTER THOUGH SINCE WE'RE IN DEVELOPMENT" }}
+  env_var_missing = -> { "xxx".tap { STDERR.puts "ENV VAR MISSING! SHOULDN'T MATTER THOUGH SINCE WE'RE IN DEVELOPMENT" } }
 
   config.paperclip_defaults = {
     storage: :s3,
