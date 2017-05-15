0f9e805fad5b701ea2ef8d8c68ec11a193cddb4b
Russell Osborne <russell@theironyard.com>
2017-05-15 10:41:18 -0400

    Add explicit dependencies for helpers inside of view specs.

--- spec/views/facilities/index.html.erb_spec.rb
+++ spec/views/facilities/index.html.erb_spec.rb
@@ -2,6 +2,15 @@ require 'rails_helper'
 
 RSpec.describe "facilities/index", type: :view do
 
+  before do
+    controller.singleton_class.class_eval do
+      include ActionView::Helpers::TagHelper
+      include ChartHelper
+      include ApplicationHelper
+      helper_method :chart, :map_chart_styles
+    end
+  end
+  
   it "renders a list of facilities" do
     assign(:facilities, [
       create(:facility, :name => "john"),
